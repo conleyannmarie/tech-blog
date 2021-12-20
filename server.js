d79421f7fe19439f9dfe632cd84c476de7288e53
Conley Marie <81729253+conleyannmarie@users.noreply.github.com>
2021-12-19 20:32:26 -0700

    first file

--- server.js
+++ server.js
@@ -1,1 +1,8 @@
-// Adding code
+const express = require('express');
+const routes = require('');
+const sequelize = require('');
+const path = require('path');
+const helpers = require('.');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
